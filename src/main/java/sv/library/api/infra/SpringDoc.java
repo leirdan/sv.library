9466e676e5335ce9808a939e2661cf251529225b
leirdan <uouleirdan@gmail.com>
2024-01-15 17:22:26 -0300

    :ok_hand: style: quebra de linha

--- src/main/java/sv/library/api/infra/SpringDoc.java
+++ src/main/java/sv/library/api/infra/SpringDoc.java
@@ -3,7 +3,6 @@ package sv.library.api.infra;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -12,17 +11,18 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringDoc {
-    @Bean
-    public OpenAPI customApi() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(
-                                "bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .info(new Info()
-                        .title("SV Library API")
-                        .description("API RESTful que controla os dados de uma biblioteca.")
-                        .license(new License().name("MIT")));
-    }
+        @Bean
+        public OpenAPI customApi() {
+                return new OpenAPI()
+                                .components(new Components()
+                                                .addSecuritySchemes(
+                                                                "bearer-key",
+                                                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")))
+                                .info(new Info()
+                                                .title("SV Library API")
+                                                .description("API RESTful que controla os dados de uma biblioteca.")
+                                                .license(new License().name("MIT")));
+        }
 }
