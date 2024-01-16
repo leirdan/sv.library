cf25a1dc90cd40c6c7d8e4d7f99e6c008857978b
leirdan <uouleirdan@gmail.com>
2024-01-16 15:26:29 -0300

    :ok_hand: refactor: removidos imports inúteis

--- src/main/java/sv/library/api/services/repository/IBookRepository.java
+++ src/main/java/sv/library/api/services/repository/IBookRepository.java
@@ -3,10 +3,7 @@ package sv.library.api.services.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
-import jakarta.transaction.Transactional;
 import sv.library.api.domain.Book;
 
 public interface IBookRepository extends JpaRepository<Book, Long> {
